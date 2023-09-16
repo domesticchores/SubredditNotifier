37ff76940e30aeb62bf15eeb5586d1af0070dbad
Akash Keshav <112591754+domesticchores@users.noreply.github.com>
2023-09-16 16:48:34 -0500

    Update rssFeed.py

--- rssFeed.py
+++ rssFeed.py
@@ -1,10 +1,9 @@
-import lxml
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
 import os
 from pathlib import Path
-from datetime import datetime, timedelta
+from datetime import datetime, timezone
 import pytz
 import schedule
 import time
@@ -17,9 +16,12 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0"
 }
 
+localTZ = datetime.now(timezone.utc).astimezone().tzinfo
+
 
 class rssFeed:
     def __init__(self, rss_url, headers):
+        print("Initiating RSS Reader...")
         self.url = rss_url
         self.headers = headers
 
@@ -27,7 +29,7 @@ class rssFeed:
             self.connection = sqlite3.connect("postData.db")
             self.cursor = self.connection.cursor()
             self.cursor.execute(
-                """CREATE TABLE IF NOT EXISTS posts(id TEXT PRIMARY KEY, title TEXT, url TEXT, pubdate TEXT)"""
+                """CREATE TABLE IF NOT EXISTS posts(id TEXT PRIMARY KEY, title TEXT, price TEXT, type TEXT, url TEXT, pubdate TEXT)"""
             )
         except Exception as e:
             print("Error connecting to Database: ")
@@ -46,13 +48,31 @@ class rssFeed:
         self.posts = self.soup.findAll("entry")
         self.postDict = [
             {
-                "title": a.find("title").text,
                 "id": a.find("id").text,
-                "url": a.find("link").get("href"),
+                "title": a.find("title").text[
+                    a.find("title").text.find("]")
+                    + 2 : a.find("title").text.find("$")
+                    - 2
+                ]
+                + " ",
+                "price": (a.find("title").text + " ")[
+                    a.find("title")
+                    .text.find("$") : a.find("title")
+                    .text.find(" ", a.find("title").text.find("$"))
+                ],
+                "type": a.find("title").text[
+                    a.find("title").text.find("[")
+                    + 1 : a.find("title").text.find("]", a.find("title").text.find("["))
+                ],
+                "url": a.find("content").text[
+                    a.find("content").text.find("<span><a href=")
+                    + 15 : a.find("content").text.find(
+                        '"', a.find("content").text.find("<span><a href=") + 15
+                    )
+                ],
                 "pubdate": datetime.strptime(
                     a.find("published").text[:19], "%Y-%m-%dT%H:%M:%S"
-                ).astimezone(pytz.timezone("America/Chicago"))
-                - timedelta(hours=5),
+                ),
             }
             for a in self.posts
         ]
@@ -67,10 +87,16 @@ class rssFeed:
                 break
         print(
             "cycle completed! ("
-            + datetime.now().strftime("%m-%d-%y @ %H:%M:%S %Z")
+            + datetime.now().strftime("%m/%d/%y @ %I:%M %p")
             + ")\n"
         )
         return
+
+    def intOrFloat(string):
+        try:
+            return float(string)
+        except Exception as e:
+            return int(string)
 
     def findPost(ID, cursor):
         cursor.execute("SELECT * FROM posts where id=:ID", {"ID": ID})
@@ -79,10 +105,12 @@ class rssFeed:
     def addPost(Post, connection, cursor):
         with connection:  # auto commits the executions that follow
             cursor.execute(
-                "INSERT INTO posts VALUES (:id, :title, :url, :pubdate)",
+                "INSERT INTO posts VALUES (:id, :title, :price, :type, :url, :pubdate)",
                 {
                     "id": Post.get("id"),
                     "title": Post.get("title"),
+                    "price": Post.get("price"),
+                    "type": Post.get("type"),
                     "url": Post.get("url"),
                     "pubdate": Post.get("pubdate"),
                 },
@@ -90,17 +118,28 @@ class rssFeed:
 
     def sendNotif(Post):
         print("New post found! ID: " + Post.get("id"))
+        linkTo = Post.get("url")
         try:
             requests.post(
                 NTFY_URL,
-                data=Post.get("url")
+                data=Post.get("title")
+                + "\n"
+                + Post.get("url")
                 + "\n("
-                + Post.get("pubdate").strftime("%m-%d-%y @ %H:%M:%S %Z")
+                + pytz.timezone("UTC")
+                .localize(Post.get("pubdate"))
+                .astimezone(tz=localTZ)
+                .strftime("%m/%d/%y @ %I:%M %p")
                 + ")",
                 headers={
-                    "Title": Post.get("title"),
+                    "Title": "New Sale: ["
+                    + Post.get("type")
+                    + ", "
+                    + Post.get("price")
+                    + "]",
                     "Priority": "default",
                     "Tags": "computer",
+                    "Actions": f"view, Open Link, {linkTo}",
                 },
             )
         except Exception as e:
@@ -109,6 +148,7 @@ class rssFeed:
 
 
 if __name__ == "__main__":
+    rssFeed(REDDIT_RSS_URL, headers)
     schedule.every(MINUTES_BEFORE_LOOP).minutes.do(rssFeed, REDDIT_RSS_URL, headers)
     while True:
         schedule.run_pending()
